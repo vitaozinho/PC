c33c11c6dbf24d6a2eb1f214822a12de430fbdb9
vitaozinho <vitor.macedo32@gmail.com>
2018-03-02 13:26:16 -0300

    Aula com operadores matematicos

---
+++ Aula2/src/operadores/operadores.java
@@ -0,0 +1,13 @@
+package operadores;
+
+public class operadores {
+	public static void main(String args[]) {
+		
+		int raio = 12;
+		double pi= 3.1415, area;
+		
+		area=pi*raio.Math.pow;
+		System.out.println("a area é:" area); 
+		
+	}
+}

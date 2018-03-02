3dd0637b07cb38f079a1b7b10022f30a76883f46
Vitor Hugo <30542437+vitaozinho@users.noreply.github.com>
2018-03-02 17:29:20 -0300

    Update operadores.java

--- Aula2/src/operadores/operadores.java
+++ Aula2/src/operadores/operadores.java
@@ -2,12 +2,14 @@ package operadores;
 
 public class operadores {
 	public static void main(String args[]) {
-		
+		//exercicio7
 		int raio = 12;
 		double pi= 3.1415, area;
 		
-		area=pi*raio.Math.pow;
-		System.out.println("a area é:" area); 
+		area=pi*Math.pow(raio,2);
+		System.out.println("a area Ã©:" area); 
+		
+		//exercicio8
 		
 	}
 }
